22158cab4732896c180b0c01063787cae9c6fd35
rsasheen <98282061+rsasheen@users.noreply.github.com>
2022-05-02 15:29:40 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
 import { StyleSheet } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/screens/Login";
+import { isAuthenticatedUser } from "./src/screens/Login";
+import GeoLocation from "./src/Location";
 import { NavigationContainer } from "@react-navigation/native";
+
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './config/keys';
 
 const Stack = createStackNavigator();
 
+var listOfSongs;
+
 export default function App() {
+  console.log(isAuthenticatedUser());
+  if (!firebase.getApps().length) {
+    console.log('Connected with Firebase')
+    firebase.initializeApp(firebaseConfig);
+  }
   return (
     <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen options={{headerShown:false}} name="Login" 
-          component={Login}/>
-        </Stack.Navigator>
+        {!true ? (
+              <Stack.Screen
+                options={{ headerShown: false }}
+                name="Login"
+                component={Login}
+              />
+            ) : (
+              <Stack.Screen
+                options={{ headerShown: false }}
+                name="Home"
+                component={GeoLocation}
+              />
+            )}
+
+      </Stack.Navigator>
       </NavigationContainer>
   );
 }
